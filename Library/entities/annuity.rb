c9646554c6791ea18f66ed0ab4ffac67ae69e1af
jsp <jspenkar@gmail.com>
2014-11-17 12:30:00 -0600

    Limited Annuity grow/no grow added.

--- Library/entities/annuity.rb
+++ Library/entities/annuity.rb
@@ -1,11 +1,11 @@
 module Finance_Engine
 	class Annuity
-		def self.present_value_perpetuity_growth(annuity, rate, growth=0)
+		def self.present_value_perpetuity(annuity, rate, growth=0)
 			annuity*(1+growth)/(rate-growth)
 		end
 
-		def self.present_value_annuity_growth(annuity, rate, periods, growth)
-			return annuity / (rate - growth) *( 1 - ((1+growth)/(1+rate))**periods ) 
+		def self.present_value_annuity(annuity, rate, periods, growth=0)
+			annuity / (rate - growth) *( 1 - ((1+growth)/(1+rate))**periods ) 
 		end
 	end
 end
--- spec/entities/annuity_spec.rb
+++ spec/entities/annuity_spec.rb
@@ -1,15 +1,17 @@
 require_relative '../spec_helper.rb'
 
 describe Finance_Engine::Annuity do 
-	it 'Should be able to find the present value of a growing, or frozen perpetuity.' do
-		a = Finance_Engine::Annuity.present_value_perpetuity_growth(100,0.05,0.02)
+	it 'Should be able to find the present value of a growing or frozen perpetuity.' do
+		a = Finance_Engine::Annuity.present_value_perpetuity(100,0.05,0.02)
 		expect(a).to be_within(0.05).of(3400)
-		b = Finance_Engine::Annuity.present_value_perpetuity_growth(100,0.05)
+		b = Finance_Engine::Annuity.present_value_perpetuity(100,0.05)
 		expect(b).to be_within(0.05).of(2000)
 	end
 
-	xit 'Should be able to find the present value of a perpetuity.' do
-		hash = {cash_flow: 100}
-		expect(a)
+	it 'Should be able to find the present value of a growing or frozen annuity.' do
+		a = Finance_Engine::Annuity.present_value_annuity(100, 0.05, 20, 0.02)
+		expect(a).to be_within(0.05).of(1466.55)
+		b = Finance_Engine::Annuity.present_value_annuity(100, 0.05, 20)
+		expect(b).to be_within(0.05).of(1246.20)
 	end
 end
