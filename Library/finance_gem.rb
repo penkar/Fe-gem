0cb3586aebb4847475d7d9ae8b494c96275030b0
jsp <jspenkar@gmail.com>
2014-11-19 08:18:18 -0600

    Small name change.

--- Library/finance_gem.rb
+++ Library/finance_gem.rb
@@ -2,7 +2,7 @@ require_relative 'entities/optionpricing.rb'
 require_relative 'entities/tvm.rb'
 require_relative 'entities/eareay.rb'
 require_relative 'entities/annuity.rb'
-require_relative 'entities/equity.rb'
+require_relative 'entities/gordon_growth_model.rb'
 
 module Finance_Engine
 	def self.record=(x)
