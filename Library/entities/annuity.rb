810ab7bf5fb638e58ca76f6d9afadfb45ff6e054
jsp <jspenkar@gmail.com>
2014-11-18 23:14:02 -0600

    add

--- Library/entities/annuity.rb
+++ Library/entities/annuity.rb
@@ -5,7 +5,7 @@ module Finance_Engine
 		end
 
 		def self.present_value_annuity(annuity, rate, periods, growth=0)
-			annuity / (rate - growth) *( 1 - ((1+growth)/(1+rate))**periods ) 
+			annuity /(rate - growth) *( 1 - ((1+growth)/(1+rate))**periods ) 
 		end
 	end
 end
