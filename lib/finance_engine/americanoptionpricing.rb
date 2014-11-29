b7e568aa715f80f19f9c854186a30b81ae567504
jsp <jspenkar@gmail.com>
2014-11-29 16:52:24 -0600

    Probability of incrase in price working

--- lib/finance_engine/americanoptionpricing.rb
+++ lib/finance_engine/americanoptionpricing.rb
@@ -1,11 +1,28 @@
 require 'distribution'
 module FinanceEngine
 	class American_Options
-		attr_accessor :price, :vol, :rf
-		def initialize(price, volatility, risk_free_rate)
+		attr_accessor :price, :vol, :rf, :strike, :time
+		def initialize(price, volatility, risk_free_rate, strike)
 			@price = price
 			@vol = volatility
 			@rf = risk_free_rate
+			@strike = strike
+			# @time = time
 		end
+
+		def create_for_years(x)
+			
+		end
+
+		def up_factor
+			Math::E**(@vol*(0))
+		end
+		def down_factor
+			Math::E**(-@vol*(0))
+		end
+		def probability_increase_price(time)
+			0.5 * (1+(@rf / @vol - @vol/2) *(time/12.0)**(0.5))
+		end
+
 	end
 end
--- spec/entities/americanoptionpricing_spec.rb
+++ spec/entities/americanoptionpricing_spec.rb
@@ -2,9 +2,18 @@ require_relative '../spec_helper.rb'
 
 describe FinanceEngine::American_Options do 
 	it 'It should be able to initialize with price, volatility and rate.' do
-		first = FinanceEngine::American_Options.new(100,0.2,0.05)
+		first = FinanceEngine::American_Options.new(100,0.2,0.05,90)
 		expect(first.rf).to eq(0.05)
 		expect(first.vol).to eq(0.2)
 		expect(first.price).to eq(100)
+		expect(first.strike).to eq(90)
 	end
+
+	it 'It should be able calculate the correct probability of an increase of price for a 1 month period.' do
+		first = FinanceEngine::American_Options.new(32,0.2,0.10,90)
+		expect(first.price).to eq(32)
+		ans = first.probability_increase_price(1)
+		expect(ans.round(4)).to be_within(0.0005).of(0.5574)
+	end
+
 end
