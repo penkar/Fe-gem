1a4c041e33884ee38972bae2750a957d8f87c2be
jsp <jspenkar@gmail.com>
2014-11-30 20:01:23 -0600

    Small syntax changes.

--- lib/finance_engine/americanoptionpricing.rb
+++ lib/finance_engine/americanoptionpricing.rb
@@ -1,29 +1,33 @@
 require 'distribution'
 module FinanceEngine
 	class American_Options
-		attr_accessor :price, :vol, :rf, :strike, :time
+		attr_accessor :price, :vol, :rf, :strike, :time, :tree
 		def initialize(price, volatility, risk_free_rate, strike)
 			@price = price
 			@vol = volatility
 			@rf = risk_free_rate
 			@strike = strike
-			# @time = time
+			@tree = {}
 		end
 
-		def create_for_years(x)
-			
+		def create_tree_for_years(price=@price, years, periods_in_year, node='original_')
+			if years.round(4) == 0
+
+			elsif year.round(4) > 0
+				
+			end
 		end
 
 		def up_factor(time)
-			Math::E**(@vol*(time/12.0)**(0.5))
+			Math::E**(@vol*(time)**(0.5))
 		end
 
 		def down_factor(time)
-			Math::E**(-@vol*(time/12.0)**(0.5))
+			Math::E**(-@vol*(time)**(0.5))
 		end
 		
 		def probability_increase_price(time)
-			0.5 * (1+(@rf / @vol - @vol/2) *(time/12.0)**(0.5))
+			0.5 * (1+(@rf / @vol - @vol/2) *(time)**(0.5))
 		end
 
 	end
