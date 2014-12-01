e82ef31199c7c85452a6b16587fe32b99b007747
jsp <jspenkar@gmail.com>
2014-12-01 12:24:50 -0600

    Restructuring up and down factors in lib and test.

--- lib/finance_engine/americanoptionpricing.rb
+++ lib/finance_engine/americanoptionpricing.rb
@@ -1,7 +1,7 @@
 require 'distribution'
 module FinanceEngine
 	class American_Options
-		attr_accessor :price, :vol, :rf, :strike, :time, :tree
+		attr_accessor :price, :vol, :rf, :strike, :time, :tree, :up_factor, :down_factor
 		def initialize(price, volatility, risk_free_rate, strike)
 			@price = price
 			@vol = volatility
@@ -10,15 +10,21 @@ module FinanceEngine
 			@tree = {}
 		end
 
-		def create_tree_for_years(years, periods_in_year, node='original_', price=@price)
+		def build_american_options(periods_in_year)
+			up_factor(1.0/periods_in_year)
+			down_factor(1.0/periods_in_year)
+		end
+
+		def create_tree_for_years(years_to_expiration, periods_in_year, node='original_', price=@price)
 			@tree[node]={'price'=>price.round(4), 'call'=> call_value(price), 'put'=>put_value(price)}
-			if years.round(3) == 0
-				@tree[node]
-			elsif years.round(3) > 0
+			if years_to_expiration.round(3) == 0
+				@tree[node]['call'] = call_value(price)
+				@tree[node]['put'] = put_value(price)
+			elsif years_to_expiration.round(3) > 0
 				price_up = price * up_factor(1.0/periods_in_year)
 				price_down = price * down_factor(1.0/periods_in_year)
-				create_tree_for_years((years - 1.0/periods_in_year), periods_in_year, node+'u', price_up, )
-				create_tree_for_years((years - 1.0/periods_in_year), periods_in_year, node+'d', price_down)
+				create_tree_for_years((years_to_expiration - 1.0/periods_in_year), periods_in_year, node+'u', price_up )
+				create_tree_for_years((years_to_expiration - 1.0/periods_in_year), periods_in_year, node+'d', price_down)
 			end
 		end
 
@@ -30,17 +36,16 @@ module FinanceEngine
 			[0,strike - price].max.round(4)
 		end
 
-		def up_factor(time)
-			Math::E**(@vol*(time)**(0.5))
+		def up_fac(time)
+			@up_factor = Math::E**(@vol*(time)**(0.5))
 		end
 
-		def down_factor(time)
-			Math::E**(-@vol*(time)**(0.5))
-		end
+		def down_fac(time)
+			@down_factor = Math::E**(-@vol*(time)**(0.5))
+		end	
 		
 		def probability_increase_price(time)
 			0.5 * (1+(@rf / @vol - @vol/2) *(time)**(0.5))
 		end
-
 	end
 end
--- spec/entities/americanoptionpricing_spec.rb
+++ spec/entities/americanoptionpricing_spec.rb
@@ -9,19 +9,15 @@ describe FinanceEngine::American_Options do
 		expect(first.strike).to eq(90)
 	end
 
-	it 'Can calc the % up a stock will go.' do
+	it 'Can calc the % up and down a stock will go.' do
 		first = FinanceEngine::American_Options.new(32,0.2,0.10,90)
-		ans = first.up_factor(1.0/12)
-		expect(ans.round(4)).to be_within(0.0005).of(1.0594)
+		ans = first.up_fac(1.0/12)
+		ans = first.down_fac(1.0/12)
+		expect(first.up_factor.round(4)).to be_within(0.0005).of(1.0594)
+		expect(first.down_factor.round(4)).to be_within(0.0005).of(0.9439)
 	end	
 
-	it 'Can calc the % down a stock will go.' do
-		first = FinanceEngine::American_Options.new(32,0.2,0.10,90)
-		ans = first.down_factor(1.0/12)
-		expect(ans.round(4)).to be_within(0.0005).of(0.9439)
-	end	
-
-	it 'It should be able calculate the correct probability of an increase of price for a 1 month period.' do
+		it 'It should be able calculate the correct probability of an increase of price for a 1 month period.' do
 		first = FinanceEngine::American_Options.new(32,0.2,0.10,90)
 		expect(first.price).to eq(32)
 		ans = first.probability_increase_price(1.0/12)
@@ -29,9 +25,9 @@ describe FinanceEngine::American_Options do
 	end
 
 	it 'Be able to create tree.' do
-		first = FinanceEngine::American_Options.new(32,0.2,0.10,90)
-		first.create_tree_for_years(2, 1)
-		puts first.tree
+		first = FinanceEngine::American_Options.new(32,0.2,0.10,35)
+		first.create_tree_for_years(1)
+		puts first
 		expect(first.tree['original_dd']['price']).to be_within(0.0005).of(21.450)
 		expect(first.tree['original_uu']['price']).to be_within(0.0005).of(47.7384)
 	end
